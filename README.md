3800aa29bfaf3351efd8d5628b99be45a91fe888
Rob Winch <rwinch@vmware.com>
2013-02-11 14:12:35 -0600

    initial

---
+++ README.md
@@ -0,0 +1,34 @@
+Introduction
+=========================
+A simple [skype4py](https://github.com/awahlig/skype4py) script that allows speed dialing with pauses and extensions.
+
+Installation
+=========================
+
+* Ensure that you have [Skype](http://www.skype.com/) installed. On Ubuntu this can be done using the following command:
+
+    sudo apt-get install skype
+
+* Ensure that you have [skype4py](https://github.com/awahlig/skype4py) installed. On Ubuntu this can be done by performing the following commands
+
+    sudo apt-get install python python-pip
+    sudo pip install --upgrade Skype4Py
+
+* Copy speeddial.py to a known location and ensure that it is executable (i.e. chmod +x speeddial.py). In the future, we will refer to this location as <speeddial-install-path>
+
+Usage
+=========================
+
+After following the Installation instructions, you can use speeddial.py as soon as you have started Skype by executing the script with the number you wish to dial. For example, I create a script named conference.sh that looks like this:
+
+    <speeddial-install-path>/speeddial.py 18001234567
+
+Now to run it all you need to do is execute the script. This isn't all that useful since you can create a contact within Skype that has a number. However, the script also supports pausing and extensions. For example, if you enter the following:
+
+    <speeddial-install-path>/speeddial.py 18001234567,,890#
+
+# The number 18001234567 is dialed
+# It waits 2 seconds (1 second for each ,). You can add as few or as many , as you like
+# It dials 890#
+
+Now you can create a shortcut (i.e. bash script, alias, etc) for each phone number you call frequently!
---
+++ speeddial.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import time
+import re
+import sys
+import Skype4Py
+import signal
+
+def handler(signum, frame):
+    if(Call):
+        Call.Finish()
+    sys.exit()
+
+signal.signal(signal.SIGINT, handler)
+
+# This variable will get its actual value in OnCall handler
+CallStatus = 0
+
+# Here we define a set of call statuses that indicate a call has been either aborted or finished
+CallIsFinished = set ([Skype4Py.clsFailed, Skype4Py.clsFinished, Skype4Py.clsMissed, Skype4Py.clsRefused, Skype4Py.clsBusy, Skype4Py.clsCancelled]);
+
+def AttachmentStatusText(status):
+   return skype.Convert.AttachmentStatusToText(status)
+
+def CallStatusText(status):
+    return skype.Convert.CallStatusToText(status)
+
+# This handler is fired when status of Call object has changed
+def OnCall(call, status):
+    global CallStatus
+    CallStatus = status
+    if call.WaitTime and status == Skype4Py.clsInProgress:
+        print 'Waiting ' + str(call.WaitTime) + ' seconds'
+        time.sleep(call.WaitTime)
+        print 'Entering Code ' + Call.Extension
+        for c in Call.Extension:
+          call.DTMF = c
+
+# This handler is fired when Skype attatchment status changes
+def OnAttach(status):
+    if status == Skype4Py.apiAttachAvailable:
+        skype.Attach()
+        
+# Let's see if we were started with a command line parameter..
+try:
+    CmdLine = sys.argv[1]
+except:
+    print 'Missing command line parameter'
+    sys.exit()
+
+
+
+# Creating Skype object and assigning event handlers..
+skype = Skype4Py.Skype()
+skype.OnAttachmentStatus = OnAttach
+skype.OnCallStatus = OnCall
+
+# Starting Skype if it's not running already..
+if not skype.Client.IsRunning:
+    print 'Starting Skype..'
+    skype.Client.Start()
+
+# Attatching to Skype..
+skype.Attach()
+
+number = re.search('(\d+)(,*)([\d#\*]*)', CmdLine)
+Call = skype.PlaceCall(number.group(1))
+Call.WaitTime = len(number.group(2))
+Call.Extension = number.group(3)
+
+#if not Found:
+#    print 'Call target not found in contact list'
+#    sys.exit()
+
+# Loop until CallStatus gets one of "call terminated" values in OnCall handler
+while not CallStatus in CallIsFinished:
+    pass
